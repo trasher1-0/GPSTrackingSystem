76d46200cf014ba52213028186492dd976fd69a1
haseeAmarathunga <haseeamarathunga@gmail.com>
2019-05-25 10:41:45 +0530

    GPS Tracking success

--- TrasherLocation.ino
+++ TrasherLocation.ino
@@ -5,17 +5,21 @@
 #include "TinyGPS++.h"
 
 // Set these to run example.
-#define FIREBASE_HOST "vehicletrackinsystem.firebaseio.com"
-#define FIREBASE_AUTH "Yy9Blaj6oxgBCo7QXKpABB8PrtNZFRN5zD4Dq0Mt"
-#define WIFI_SSID "Hasee"
-#define WIFI_PASSWORD "haseesart"
+#define FIREBASE_HOST "trasher-5a2fd.firebaseio.com"
+#define FIREBASE_AUTH "iFn8z0b8wYte0LtcgMcNPbqIvzV3dbfghFmCa8TY"
+#define WIFI_SSID "BELL4G-LTE"
+#define WIFI_PASSWORD "leavemealone12"
 
 TinyGPSPlus gps;
 SoftwareSerial ss(4,5);
+String f;
+float lat;
+float lng;
 
 void setup() {
-  Serial.println("GPS Start");
   Serial.begin(9600);  
+  ss.begin(9600);
+  
   // connect to wifi.
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.print("connecting");
@@ -26,17 +30,31 @@ void setup() {
   Serial.println();
   Serial.print("WiFi Connected: ");
   Serial.println(WiFi.localIP());
+  Serial.println("GPS Start");
   
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
 }
 void loop() {
-   while (ss.available() > 0)
-    if (gps.encode(ss.read()))
-    {
-      if (gps.location.isValid())
-      {
-        Serial.println("Latitude : "+String(gps.location.lat() , 6));
-        Serial.println("Longitude : "+String(gps.location.lng() , 6));
-      }
+  while(ss.available()){
+    gps.encode(ss.read());
+  }
+  
+  if(gps.location.isUpdated())
+  {
+    if (Firebase.failed()) {
+       Serial.print("setting /location failed:");
+       Serial.println(Firebase.error());
+       Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+       delay(2000);  
+       return;
     }
+
+    lat=gps.location.lat();
+    lng=gps.location.lng();
+    Firebase.setFloat("coords/azhsgsfskc1x/latitude",lat);
+    Firebase.setFloat("coords/azhsgsfskc1x/longitude",lng);
+    f=String(lat,6)+","+String(lng,6);
+    Serial.println(f);
+    delay(10000); 
+  } 
 }
